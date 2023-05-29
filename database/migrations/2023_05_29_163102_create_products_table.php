7cc64c62139d85a84714adefbb802d9fcccf6324
rlf09360128 <90850166+rlf09360128@users.noreply.github.com>
2023-05-29 12:26:16 -0600

    Se agregan nuevos modelos

--- database/migrations/2023_05_29_163102_create_products_table.php
+++ database/migrations/2023_05_29_163102_create_products_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("name",30)->nullable(false);
+            $table->string("decription",50)->nullable();
+            $table->string("sku",8)->unique();
+            $table->float("price")->default(0);
+            $table->smallInteger("cantidad")->default(0);
+            $table->unsignedBigInteger("category_id")->nullable();
+            $table->boolean("status")->default(true);
+            $table->dateTime("created_at")->default(now());
+            $table->foreign('category_id')->references("id")->on("categories")->onDelete("set null");
+
+           # $table->timestamps();
         });
     }
 
