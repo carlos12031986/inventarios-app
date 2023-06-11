adcfdee61c47ac6d96600cb5d7e848452cc942d7
rlf09360128 <90850166+rlf09360128@users.noreply.github.com>
2023-06-11 10:37:34 -0600

    bdjafhjasgh

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,11 +8,43 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-   
-    public function all(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-            return null;
+        //
     }
 
-    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
 }
